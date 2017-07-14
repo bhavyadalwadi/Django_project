f5e8776ea8b8a25cff7a722fe669a7daea3ddd7d
Bhavya Dalwadi <bhavyadalwadi@gmail.com>
2017-07-14 10:24:10 -0700

    Stock Site
    ~try 1
    Stock Site

---
+++ Stock_site/stocks/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from .models import Stock
+
+class StockSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Stock
+        #fields = ('ticker', 'volume')  if you want to print all instead of this specific values
+        fields = '__all__'
